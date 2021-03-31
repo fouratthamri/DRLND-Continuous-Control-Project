daece40cd9042ae8d42380004aa695a7d54d3f12
Fourat <f.thamri@instadeep.com>
2021-03-31 08:36:56 +0100

    Added repo base files and implemented actor and critic nets

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # DRLND-Continuous-Control-Project
 The github submission of the third project of Udacity's nanodegree program entitled "Continuous Control"
+
+## Requirements
+
+To set up your python environment to run the code in this repository, follow the instructions below.
+
+1. Create (and activate) a new environment with Python 3.6.
+
+    - __Linux__ or __Mac__:
+
+    ```bash
+    conda create --name drlnd python=3.6
+    source activate drlnd
+    ```
+
+    - __Windows__:
+
+    ```bash
+    conda create --name drlnd python=3.6 
+    activate drlnd
+    ```
+
+2. Clone the repository (if you haven't already!), and navigate to the `python/` folder.  Then, install several dependencies.
+
+    ```bash
+    cd python
+    pip install .
+    ```
+
+3. On Anaconda
+
+    ```bash
+    conda install pytorch=1.7.0 -c pytorch
+    ```
+
+4. Then create an [IPython kernel](http://ipython.readthedocs.io/en/stable/install/kernel_install.html) for the `drlnd` environment.
+
+    ```bash
+    python -m ipykernel install --user --name drlnd --display-name "drlnd"
+    ```
+
+## Deep Deterministic Gradient Policy (DDPG)
+
+![Image 1](doublejointcontrol.gif?style=centerme)
+
+The goal of this project is to train a double-jointed robot arm to move to target locations in a 3D unity environment.
+
+### States
+
+The observation space consists of 33 variables corresponding to position, rotation, velocity, and angular velocities of the arm.
+
+### Actions
+
+ Each action is a vector with four real numbers, corresponding to torque applicable to two joints. Every entry in the action vector should be a number between -1 and 1.
+
+### Reward
+
+ A reward of +0.1 is provided for each step that the agent's hand is in the goal location. Thus, the goal of your agent is to maintain its position at the target location for as many time steps as possible.
+
+The task is continuous, and in order to solve the environment, the agent must get an average score of +30 over 100 consecutive episodes.
+
+### Report
+
+A full description of the Deep RL model used can be found in the file report.pdf.
---
+++ nets.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
+
+class Actor(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, hidden_size=256):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            hidden_size (int): Number of nodes in the hidden layer
+        """
+        super(Actor, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.linear1 = nn.Linear(state_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, action_size)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.linear1.weight.data.uniform_(*hidden_init(self.linear1))
+        self.linear2.weight.data.uniform_(*hidden_init(self.linear2))
+        self.linear3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state):
+        """Actor net forward pass. Takes a state as a torch tensor"""
+
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+        x = F.tanh(self.linear3(x))
+
+        return x
+
+
+class Critic(nn.Module):
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size, seed, hidden_size=256):
+        """Initialize parameters and build model.
+        Args
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            hidden_size (int): Number of nodes in the hidden layer
+        """
+        super(Critic, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.linear1 = nn.Linear(state_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, 1)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.linear1.weight.data.uniform_(*hidden_init(self.linear1))
+        self.linear2.weight.data.uniform_(*hidden_init(self.linear2))
+        self.linear3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state, action):
+        """Critic net forward pass. Takes a state as a torch tensor"""
+        xs = F.relu(self.linear1(state))
+        x = torch.cat((xs, action), dim=1)
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
+
+        return x
